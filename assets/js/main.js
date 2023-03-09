5041955ce53e70cc1bf70e54f7775e59236b7317
IngridsSilveira <ingridsouzaok@gmail.com>
2023-03-09 11:50:21 -0300

    add responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-const btn_printer = document.getElementById("printer")
+const btn_printer = document.getElementById("printer");
+
 btn_printer.addEventListener("click", (evt) => {
-    const pergunta = prompt(`Você deseja visualizar a página em PDF?
-    Responda S/N`)
-    if(pergunta === "S"){
-        window.open("https://drive.google.com/file/u/1/d/1SO9LrWxJwX7IyjkQrkuS8xjXsidyb4MM/view?usp=share_link", "_blank")
-    } else {
-        alert("Operação cancelada!!")
-    }
-})
+    alert("Você será redirecionado!!")
+  setTimeout(() => {
+    window.open(
+      "https://drive.google.com/file/u/1/d/1SO9LrWxJwX7IyjkQrkuS8xjXsidyb4MM/view?usp=share_link",
+      "_blank"
+    );
+  }, 3000);
+});
